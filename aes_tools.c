4bfad8ba65c276f8ced3d4d80cc3b9c095341def
saivishnu725 <saivishnu725@gmail.com>
2024-02-07 19:14:59 +0530

    key generator

---
+++ aes_tools.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+
+void generate_key(char *key, int size) {
+    int index;
+    char *charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    srand(time(NULL));
+    for (int i = 0; i < size; i++) {
+
+        index = rand() % 62;
+        key[i] = charset[index];
+    }
+    key[size] = '\0';
+}
