9c6731289a12af701f422ba1ba19dccb01755a16
saivishnu725 <saivishnu725@gmail.com>
2024-02-07 21:07:26 +0530

    getting keys is done

--- main.c
+++ main.c
@@ -1,71 +1,58 @@
 #include "aes_tools.h"
 #include <openssl/aes.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char *argv[]) {
     // ./main <filename> <key size> <key>
-    if (argc < 4) {
+    // interactive console -> ./main <file>
+    bool key_exists = false;
+    // printf("argc = %d\n", argc);
+    if (argc != 4 && argc != 2) {
         printf("Usage: %s <file> <key size> <key>\n", argv[0]);
+        printf("\t\t (or, for interactive console) \nUsage: %s <file>\n", argv[0]);
         return 1;
-    } else if (argc > 4) {
-        printf("Too many arguments\n");
-        return 1;
+    } else if (argc == 4) {
+        key_exists = true;
     }
+    // file name
+    char *filename = argv[1];
+    // printf("filename = %s\n", filename);
 
     // key size in bits
-    int key_size_bits = atoi(argv[1]);
+    int key_size_bits;
+    if (!key_exists) {
+        printf("Enter the key size(128, 192, 256): ");
+        scanf("%d", &key_size_bits);
+    } else {
+        key_size_bits = atoi(argv[2]);
+    }
+    // printf("key_size_bits = %d\n", key_size_bits);
+
+    // key size in bits
     if (key_size_bits != 128 && key_size_bits != 192 && key_size_bits != 256) {
         printf("Key size must be 128, 192 or 256\n");
         return 1;
     }
-
     // key size in bytes
     int key_size = bitoby(key_size_bits);
+    // printf("Key size: %d\n", key_size);
 
     // ask if they have a key or should it be generated
-    unsigned char *aesKey = malloc(key_size);
-    char key_exists;
-    printf("Do you have a %d-bit key? (y/n): ", key_size_bits);
-    scanf("%c", &key_exists);
-    if (key_exists == 'y' || key_exists == 'Y') {
-        /* code to accept the existing key */
-        printf("Please enter the key: ");
-        scanf("%s", aesKey);
-        // fgets(aesKey, key_size - 1, stdin);
-    } else {
+    unsigned char *aesKey = argv[3];
+
+    if (!key_exists) {
+        // aesKey = malloc(key_size);
         /* code to generate a key*/
         generate_key(aesKey, key_size);
     }
     printf("Key: %s\n", aesKey);
 
+    // exit the program
     return 0;
 }
-//     // ask for key size
-//     printf("Key size (128, 192, 256): ");
-//     int key_size;
-//     scanf("%d", &key_size);
-
-//     printf("Key size: %d\n", key_size);
-//     if (key_size != 128 || key_size != 192 || key_size != 256)
-//     {
-//         printf("Key size: %d\n", key_size);
-//         printf("Invalid key size\n");
-//         return 1;
-//     }
-
-//     // ask if they have a key or should I randomly generate one
-//     char *key;
-//     printf("Do you have a key? (y/n): ");
-//     char existing;
-//     scanf("%c", &existing);
-//     if (existing == 'y' || existing == 'Y')
-//         scanf("%s", key);
-//     else
-//         key = generate_key(key_size);
-
-//     printf("%s", key);
 
 //     // open the file
 //     FILE *fp = fopen(argv[1], "r");
@@ -79,6 +66,3 @@ int main(int argc, char *argv[]) {
 //     {
 //     }
 //     fclose(fp);
-
-//     return 0;
-// }
